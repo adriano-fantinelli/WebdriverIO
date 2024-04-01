d752ddee30eff9695aacb3fa48b7c39291481c84
Adriano Fantinelli <adrianofa2013@gmail.com>
2024-03-31 21:52:48 -0300

    Git init.

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,10 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page.js'
+import SecurePage from '../pageobjects/secure.page.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        
+    })
+})
+
